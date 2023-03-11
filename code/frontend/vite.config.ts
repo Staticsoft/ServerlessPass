c6fbf71a7801b90e63fb30ae326afa8d3b270af5
Pavel <pavel.oparin@myget-it.com>
2023-03-11 15:48:35 +0200

    Frontend cleanup.

--- code/frontend/vite.config.ts
+++ code/frontend/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import sassDts from 'vite-plugin-sass-dts'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import sassDts from 'vite-plugin-sass-dts';
 
 export default defineConfig({
   build: {
@@ -10,4 +10,4 @@ export default defineConfig({
     react(),
     sassDts()
   ]
-})
+});
